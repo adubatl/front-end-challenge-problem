40e6f56ff973cf8b207ffffea96114a841e9bb38
Andrew Bogle <abogle@axio.com>
2020-09-19 14:34:01 -1000

    add nav begin to style the actual view in a reasonable manner

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Equipment } from "../equipment";
 import { EquipmentService } from "../equipment.service";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 
 @Component({
     selector: "app-equipment",
@@ -9,16 +10,22 @@ import { EquipmentService } from "../equipment.service";
 })
 export class EquipmentComponent implements OnInit {
     equipment: Equipment[];
-
+    name;
     getEquipment(): void {
         this.equipmentService
             .getEquipment()
             .subscribe((equipment) => (this.equipment = equipment));
     }
 
-    constructor(private equipmentService: EquipmentService) {}
+    constructor(
+        private equipmentService: EquipmentService,
+        private route: ActivatedRoute
+    ) {}
 
     ngOnInit() {
         this.getEquipment();
+        this.route.queryParams.subscribe((params) => {
+            this.name = params["name"];
+        });
     }
 }
